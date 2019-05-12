1dd040eb4472375f80b6c1019f516ba595faff92
blucky36 <blucky36@gmail.com>
2019-05-12 14:14:11 -0600

    initial commit Happy mothers day!

---
+++ public/javascripts/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  document.getElementById("action-button").addEventListener("click",()=>{
+    window.location.href = "https://www.bustle.com/p/7-funny-mothers-day-2019-memes-that-even-mom-will-find-hilarious-17307637"
+  })
+})
